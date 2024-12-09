c9189a8f91e85c057a0e12b8446252e1869b36a3
Denis Mikaya <denis.mikaya@unblu.com>
2024-12-09 09:09:46 +0100

    initial commit

---
+++ app/src/main/java/com/unblu/navigation/demoapp/unblu/UnbluConstants.kt
@@ -0,0 +1,8 @@
+package com.unblu.navigation.usability.unblu
+
+class UnbluConstants {
+    companion object {
+        const val ENDPOINT_URL = ""
+        const val ENDPOINT_API_KEY = ""
+    }
+}
